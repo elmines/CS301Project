46fa1d6544efb320008e75dea4d059b57460c884
elmines <elmines325@gmail.com>
2018-11-11 22:29:42 -0600

    Initialize ASP.NET MVC project

---
+++ UAMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UAMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
