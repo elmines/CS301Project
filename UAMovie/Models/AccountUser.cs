781595c5302a6ae50f419baaeb0f693d73ff7ea8
bdjones13 <bdjones13@crimson.ua.edu>
2018-11-14 15:49:46 -0600

    Created Scaffolding for AccountUserController and views
    It takes data and can pass it to another method

---
+++ UAMovie/Models/AccountUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UAMovie.Models
+{
+    public class AccountUser
+    {
+        public String Username { get; set; }
+        public String EmailAddress { get; set; }
+        public String Password { get; set; }
+    }
+}
