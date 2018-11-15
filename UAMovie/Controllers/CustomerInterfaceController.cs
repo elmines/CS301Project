33eec777a0e11d1483b12b287877a76cc36ecab8
elmines <elmines325@gmail.com>
2018-11-15 02:42:26 -0600

    Add Now Playing list

---
+++ UAMovie/Controllers/CustomerInterfaceController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UAMovie.Controllers
+{
+    public class CustomerInterfaceController : Controller
+    {
+        public IActionResult NowPlaying()
+        {
+            return View();
+        }
+    }
+}
