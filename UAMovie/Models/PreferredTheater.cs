2fa9374e9d5a15c21562c3ec26f7671845bdb7d1
bdjones13 <bdjones13@crimson.ua.edu>
2018-11-19 12:37:48 -0600

    Made all the Model Classes consistent with the create table queries

---
+++ UAMovie/Models/PreferredTheater.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UAMovie.Models
+{
+    public class PreferredTheater
+    {
+        public String TheaterID { get; set; }
+        public String Username { get; set; }
+    }
+}
---
+++ UAMovie/Models/TicketOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UAMovie.Models
+{
+    public class TicketOrder
+    {
+        public String ID { get; set; }
+        public String ShowingID { get; set; }
+        public String Username { get; set; }
+        public String CardNumber { get; set; }
+        public float Cost { get; set; }
+        public int ChildTickets { get; set; }
+        public int AdultTickets { get; set; }
+        public int SeniorTicket { get; set; }
+        public String Status { get; set; }
+    }
+}
