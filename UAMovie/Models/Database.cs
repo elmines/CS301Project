29220921979f70a5f4f7c07130cfa9fcd23c153f
bdjones13 <bdjones13@crimson.ua.edu>
2018-11-19 12:09:56 -0600

    testing to make sure my gitignore successfully excludes the database class

--- UAMovie/Models/Database.cs
+++ UAMovie/Models/Database.cs
@@ -15,7 +15,7 @@ namespace UAMovie.Models
         private String connString {get;set;}
     public Database()
         {
-            connString = "DATA SOURCE=vrbsky-oracle.ua-net.ua.edu:1521/xe;PERSIST SECURITY INFO=True;USER ID=BDJONES13;Password=123456789";
+            connString = "DATA SOURCE=vrbsky-oracle.ua-net.ua.edu:1521/xe;PERSIST SECURITY INFO=True;USER ID=BDJONES13;Password=99999";
             conn = new OracleConnection();
             conn.ConnectionString = connString;
             conn.Open();
