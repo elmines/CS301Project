158535e7778cfb768b69a60786263400565ea56b
bdjones13 <bdjones13@crimson.ua.edu>
2018-11-28 10:49:35 -0600

    minor

--- UAMovie/Models/AccountUser.cs
+++ UAMovie/Models/AccountUser.cs
@@ -15,7 +15,7 @@ namespace UAMovie.Models
         public void insert()
         {
             Database db = new Database();
-            OracleCommand cmd = new OracleCommand();
+            OracleCommand cmd = new OracleCommand();    
             cmd.Connection = db.conn;
             String insertQuery = "INSERT INTO AccountUser (username,emailaddress,password) VALUES('" + this.Username + "','" + this.EmailAddress + "','" + this.Password + "')";
             cmd.CommandText = insertQuery;
