337666b8db2751fbeffd509323ef39e476ca4cc7
elmines <elmines325@gmail.com>
2018-12-03 15:34:37 -0600

    Add PreferredTheaters page

--- UAMovie/Controllers/MyAccountController.cs
+++ UAMovie/Controllers/MyAccountController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UAMovie.Models.ViewModels;
+
+using Oracle.ManagedDataAccess.Client;
+using UAMovie.Models;
+
+
 
 namespace UAMovie.Controllers
 {
@@ -23,9 +29,27 @@ namespace UAMovie.Controllers
             return View();
         }
 
-        public IActionResult PreferredTheater()
+        public IActionResult PreferredTheater(String username)
         {
-            return View();
+            TheaterViewModel theaters = new TheaterViewModel();
+            theaters.userName = username;
+            theaters.GetPreferredTheaters();
+            return View("~/Views/MyAccount/PreferredTheater.cshtml", theaters);
+        }
+
+        public ActionResult RemoveTheater(String username, String theaterID)
+        {
+            Database db = new Database();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = db.conn;
+
+            cmd.CommandText = String.Format("DELETE FROM PreferredTheater WHERE Username='{0}' AND TheaterID='{1}'",
+                username, theaterID);
+            cmd.ExecuteNonQuery();
+            cmd.Dispose();
+            db.Dispose();
+
+            return RedirectToAction("PreferredTheater", "MyAccount", new { username = username });
         }
 
         public IActionResult OrderHistory()
